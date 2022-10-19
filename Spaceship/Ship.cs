6cfc183d6ed06038cc945945cad70be1fa7f8218
Sean Stoneburner <litholight@gmail.com>
2022-10-19 08:41:57 -0700

    create ship class

---
+++ Spaceship/Ship.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceship
+{
+    class Ship
+    {
+        public Vector2 position = new Vector2(100, 100);
+    }
+}
